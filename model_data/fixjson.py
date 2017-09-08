b121b9a10190e9ad21f238ac7825fb18517f2e50
Steven Boada <stevenboada@gmail.com>
2017-09-08 10:36:25 -0400

    add docstring

--- model_data/fixjson.py
+++ model_data/fixjson.py
@@ -3,6 +3,13 @@ import pandas as pd
 import sys
 
 def fix(jsonFile):
+    ''' This reformats the JSON files that I got from the troop_creator repo.
+    They are in a slightly different format which isn't super useful for data
+    analysis. Running this script (only once) on each of the model files gets
+    them ready to be imported into pandas.
+
+    '''
+
     with open(jsonFile) as data_file:
         data = json.load(data_file)
 
